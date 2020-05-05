5e46805a6958cc5ad556220b5873db2ae7d45285
Hadrien <hadrien.sprumont@gmail.com>
2020-05-05 19:17:08 +0200

    Conveyor_bot clean

--- conveyor_bot.c
+++ conveyor_bot.c
@@ -7,8 +7,6 @@
 #include "ch.h"
 #include "hal.h"
 
-#include <chprintf.h>
-#include <usbcfg.h>
 #include <main.h>
 #include <math.h>
 
@@ -18,15 +16,25 @@
 
 //-----------------------------------------------------defines-------------------------------------------------------------
 
-#define ROTATION_SPEED		3
-#define MOVE_SPEED			6
+#define SMALL_RADIUS	15
+#define MEDIUM_RADIUS	20
+#define LARGE_RADIUS	25
+#define E_PUCK_RADIUS 	40
+#define BACK_UP_DIST	60
+
+#define COMPLETE_TURN_ERROR 5
+#define COMPLETE_TURN	360
+#define MAX_ANGLE		180
+#define SKIP_OBJ_ANGLE	20
+
+#define ROTATION_SPEED	3
+#define MOVE_SPEED		6
+
+#define SMALL_DRAG		1.0014f
+#define MEDIUM_DRAG		1.0141f
+#define LARGE_DRAG		1.0141f
 
 #define CENTER_MAX_ERROR 	10
-#define COMPLETE_TURN		360
-#define MAX_ANGLE			180
-#define E_PUCK_RADIUS 		40
-
-#define SKIP_OBJ_ANGLE		20
 
 #define STABILITY_THRESHOLD	10
 
@@ -106,8 +114,7 @@ void update_coordinates(position_t * tableau, int16_t dist, int16_t angle) {
 				while(tableau[i].angle < -MAX_ANGLE) tableau[i].angle += COMPLETE_TURN;
 			}else tableau[i].angle = 0;
 		}
-
-	}chprintf((BaseSequentialStream *)&SD3, "Dist = %d mm\r", tableau[ORIGIN].dist);
+	}
 }
 
 /**
@@ -117,8 +124,10 @@ static THD_WORKING_AREA(waConveyorBot, 512);
 static THD_FUNCTION(ConveyorBot, arg) {
 
     chRegSetThreadName(__FUNCTION__);
-    position_t *obj_pos = (position_t *)arg;
+    (void)arg;
 
+
+	static position_t obj_pos[NB_GAMEOBJECT];
     for(uint8_t i = 0; i < NB_GAMEOBJECT; i++){
     	obj_pos[i].angle = 0;
     	obj_pos[i].dist = 0;
@@ -126,28 +135,22 @@ static THD_FUNCTION(ConveyorBot, arg) {
 
     gameState_t state = ACQUISITION;
 
-    gameObject_t object;
-    gameObject_t target;
-    object = SMALL_OBJ;
-    target = RED_TGT;
+    //Initial couple of targets
+    gameObject_t object = SMALL_OBJ;
+    gameObject_t target = RED_TGT;
 
-    //bool found_object = false;
-    bool turn_complete = false;
-    bool all_obj = false;
-    //int16_t offset = 0;
-    int16_t current_angle = 0;
-    //int8_t stability_cnt = 0;
-
+    //Coefficients for the drag correction when rotating
     float drag[3];
-    drag[0] = 1.0014f;
-    drag[1] = 1.0141f;
-    drag[2] = 1.0141f;
+    drag[0] = SMALL_DRAG;
+    drag[1] = MEDIUM_DRAG;
+    drag[2] = LARGE_DRAG;
 
     uint8_t radius[3];
-    radius[0]=15;
-    radius[1]=20;
-    radius[2]=25;
+    radius[0] = SMALL_RADIUS;
+    radius[1] = MEDIUM_RADIUS;
+    radius[2] = LARGE_RADIUS;
 
+    //Sets origin of rotation
     mvt_calibrate();
 	wa_camera_enable(true);
 
@@ -155,19 +158,19 @@ static THD_FUNCTION(ConveyorBot, arg) {
     	switch(state){
     	case ACQUISITION :{ //The robot looks around to find the gameObjects
 
-			current_angle = mvt_get_angle();
-			turn_complete = current_angle < 0 && current_angle > -5;
+    		int16_t current_angle = mvt_get_angle();
+			bool turn_complete = current_angle < 0 && current_angle > -COMPLETE_TURN_ERROR;
+
 			if(turn_complete){
 				mvt_stop();
-				all_obj = true;
-				for(uint8_t i = 0; i < NB_GAMEOBJECT-1; i++){
-				    	if(obj_pos[i].dist <= 15) all_obj = false;
-				}
-				if(all_obj){
+				bool all_obj_stored = true;
+				//Ensure all objects are stored before continuing
+				for(uint8_t i = 0; i < NB_GAMEOBJECT-1; i++)
+					if(obj_pos[i].dist <= E_PUCK_RADIUS) all_obj_stored = false;
+				if(all_obj_stored){
 					state++;
-					update_coordinates(obj_pos,0,mvt_get_angle());
-					object = SMALL_OBJ;
-					target = RED_TGT;
+					//Sets all coordinates to be relative to the robot's position
+					update_coordinates(obj_pos, 0, mvt_get_angle());
 				}
 				else mvt_set_speed(ROTATION_SPEED, -ROTATION_SPEED);
 			}else mvt_set_speed(ROTATION_SPEED, -ROTATION_SPEED);
@@ -176,8 +179,8 @@ static THD_FUNCTION(ConveyorBot, arg) {
 
 			if(wa_getObject()){
 				center_on_target();
-				wa_store_object(obj_pos,mvt_get_angle());
-				mvt_rotate(SKIP_OBJ_ANGLE,ROTATION_SPEED);
+				wa_store_object(obj_pos, mvt_get_angle());
+				mvt_rotate(SKIP_OBJ_ANGLE, ROTATION_SPEED);
 				mvt_wait_end_of_movement();
 			}
 			break;
@@ -185,17 +188,17 @@ static THD_FUNCTION(ConveyorBot, arg) {
     	case TAKE_OBJECT_1 :
     	case TAKE_OBJECT_2 :
     	case TAKE_OBJECT_3 :{
-    		//printcoor(obj_pos);
-    		mvt_rotate(obj_pos[object].angle,ROTATION_SPEED);
-    		update_coordinates(obj_pos,0,obj_pos[object].angle);
+    		mvt_rotate(obj_pos[object].angle, ROTATION_SPEED);
+    		update_coordinates(obj_pos, 0, obj_pos[object].angle);
     		mvt_wait_end_of_movement();
 
     		wa_camera_enable(true);
     		center_on_target();
     		wa_camera_enable(false); //Disable camera while moving
 
-    		mvt_move((obj_pos[object].dist-E_PUCK_RADIUS-radius[object-3]),MOVE_SPEED);
-    		update_coordinates(obj_pos,obj_pos[object].dist-E_PUCK_RADIUS-radius[object-3],0);
+    		int16_t dist = obj_pos[object].dist - E_PUCK_RADIUS - radius[object-3];
+    		mvt_move(dist, MOVE_SPEED);
+    		update_coordinates(obj_pos, dist, 0);
 			mvt_wait_end_of_movement();
     		state++;
     		break;
@@ -203,17 +206,18 @@ static THD_FUNCTION(ConveyorBot, arg) {
     	case MOVE_TO_TARGET_1 :
     	case MOVE_TO_TARGET_2 :
     	case MOVE_TO_TARGET_3 :{
-    		//printcoor(obj_pos);
-    		mvt_rotate(obj_pos[target].angle*drag[object-3],ROTATION_SPEED);
-    		update_coordinates(obj_pos,0,obj_pos[target].angle);
+
+    		mvt_rotate(obj_pos[target].angle * drag[object-3], ROTATION_SPEED);
+    		update_coordinates(obj_pos, 0, obj_pos[target].angle);
 			mvt_wait_end_of_movement();
 
-			mvt_move((obj_pos[target].dist-E_PUCK_RADIUS-radius[object-3]),MOVE_SPEED);
-			update_coordinates(obj_pos,obj_pos[target].dist-E_PUCK_RADIUS-radius[object-3],0);
+			int16_t dist = obj_pos[target].dist - E_PUCK_RADIUS - radius[object-3];
+			mvt_move(dist, MOVE_SPEED);
+			update_coordinates(obj_pos, dist, 0);
 			mvt_wait_end_of_movement();
 
-			mvt_move(-60,MOVE_SPEED);
-			update_coordinates(obj_pos,-60,0);
+			mvt_move(-BACK_UP_DIST, MOVE_SPEED);
+			update_coordinates(obj_pos, -BACK_UP_DIST, 0);
 			mvt_wait_end_of_movement();
 
 			object++;
@@ -222,32 +226,24 @@ static THD_FUNCTION(ConveyorBot, arg) {
     		break;
     	}
     	case END_OF_TASK :
-    		mvt_rotate(obj_pos[ORIGIN].angle,ROTATION_SPEED);
+    		mvt_rotate(obj_pos[ORIGIN].angle, ROTATION_SPEED);
     		mvt_wait_end_of_movement();
-    		chprintf((BaseSequentialStream *)&SD3, "Consigne : dist = %d mm\r", obj_pos[ORIGIN].dist);
-    		mvt_move(obj_pos[ORIGIN].dist,MOVE_SPEED);
+    		mvt_move(obj_pos[ORIGIN].dist, MOVE_SPEED);
     		mvt_wait_end_of_movement();
-    		mvt_set_speed(ROTATION_SPEED,-ROTATION_SPEED);
+    		mvt_set_speed(ROTATION_SPEED, -ROTATION_SPEED);
     		state++;
     		break;
     	default :
     		palTogglePad(GPIOB, GPIOB_LED_BODY);
     		chThdSleepMilliseconds(500);
     	}
-
-
-    	//chprintf((BaseSequentialStream *)&SDU1, "Dist = %d mm\r", mm);
-
-
     }
 }
 
 void conveyor_bot_init(void){
 
-	static position_t objects_pos[NB_GAMEOBJECT];
-
 	mvt_init();
 	world_analysis_start();
 
-	chThdCreateStatic(waConveyorBot, sizeof(waConveyorBot), NORMALPRIO, ConveyorBot, objects_pos);
+	chThdCreateStatic(waConveyorBot, sizeof(waConveyorBot), NORMALPRIO, ConveyorBot, NULL);
 }
